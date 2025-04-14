e9182173d9e13334bd652902eb2d7bd02ab7d802
Ryan <rmbackman@gmail.com>
2025-04-13 21:13:19 -0700

    ignore default fallback font

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+window.addEventListener('load', () => {
+	document.body.classList.add('loaded');
+});
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<App />
