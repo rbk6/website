28ecdf0bd213617ea0d7735c84849df3edaee695
Ryan <rmbackman@gmail.com>
2025-04-26 15:36:25 -0700

    add tab interface and map tabs

--- src/components/Browser/Tabs/index.tsx
+++ src/components/Browser/Tabs/index.tsx
@@ -1,15 +1,26 @@
+import { useState } from 'react';
 import './index.scss';
 
 interface Props {
 	path: string;
 }
 
+interface Tab {
+	path: string;
+	key?: string;
+}
+
 function Tabs({ path }: Props) {
+	const defaultTabs: Tab[] = [{ path: '/' }, { path: '/about' }, { path: '/ projects' }];
+	const [tabs] = useState<Tab[]>(defaultTabs);
+
 	return (
 		<div className="tabs">
-			<div className="tab">
-				<span className="title">{path == '/' ? 'home' : path.substring(1)}</span>
-			</div>
+			{tabs.map((tab, index) => (
+				<div key={index} className={`tab ${path === tab.path ? 'active' : 'inactive'}`}>
+					<span className="title">{tab.path == '/' ? 'home' : tab.path.substring(1)}</span>
+				</div>
+			))}
 			<div className="controls">
 				<div className="maximize btn" />
 				<div className="minimize btn" />
