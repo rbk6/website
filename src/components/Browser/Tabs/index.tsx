1535376405c9f3ac1d70b26e1cefdaf16012776b
Ryan <rmbackman@gmail.com>
2025-04-26 21:51:04 -0700

    add create/close tab logic

--- src/components/Browser/Tabs/index.tsx
+++ src/components/Browser/Tabs/index.tsx
@@ -12,23 +12,40 @@ interface Tab {
 
 function Tabs({ path }: Props) {
 	const defaultTabs: Tab[] = [{ path: '/' }, { path: '/about' }, { path: '/ projects' }];
-	const [tabs] = useState<Tab[]>(defaultTabs);
+	const [tabs, setTabs] = useState<Tab[]>(defaultTabs);
+
+	const createTab = () => setTabs([...tabs, { path: '/new', key: `new-${tabs.length}` }]);
+	const closeTab = (curr: Tab) => {
+		if (curr.key === undefined) {
+			setTabs(tabs.filter((tab) => tab.path !== curr.path));
+		} else {
+			setTabs(tabs.filter((tab) => tab.key !== curr.key));
+		}
+	};
 
 	return (
 		<div className="tabs">
 			<div className="tabs-left">
-				{tabs.map((tab, index) => (
-					<div
-						key={index}
-						className={`tab ${path === tab.path ? 'active' : 'inactive'}`}
-						title={`https://rbk6.dev${tab.path}`}
-					>
-						<span className="title">{tab.path == '/' ? 'home' : tab.path.substring(1)}</span>
-						<button className="close-tab-btn">x</button>
-					</div>
-				))}
+				<div style={{ display: 'flex', alignItems: 'center', overflow: 'hidden', margin: 0 }}>
+					{tabs.map((tab, index) => (
+						<div
+							key={index}
+							className={`tab ${path === tab.path ? 'active' : 'inactive'}`}
+							title={tab.key === undefined ? `https://rbk6.dev${tab.path}` : 'new tab'}
+						>
+							<span className="title">
+								{tab.path == '/' ? 'home' : tab.path == '/new' ? 'new tab' : tab.path.substring(1)}
+							</span>
+							<button onClick={() => closeTab(tab)} className="close-tab-btn">
+								x
+							</button>
+						</div>
+					))}
+				</div>
 				<div className="new-tab">
-					<button className="new-tab-btn">+</button>
+					<button onClick={() => createTab()} className="new-tab-btn">
+						+
+					</button>
 				</div>
 			</div>
 			<div className="tabs-right">
